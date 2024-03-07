aac553a890333e7b5884e698745618fdd160a929
John Danial <john.danial@me.com>
2024-03-07 08:25:37 -0500

    Initial Commit

---
+++ ContactPicker/ContactPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactPickerApp.swift
+//  ContactPicker
+//
+//  Created by johndanial on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
